86671168dc502ee7d4eda26e4190d04f9b8dd60d
Deepak Bhagat <149673145+deepak-bhagat-iitr@users.noreply.github.com>
2024-03-13 17:03:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const collection = require("./mongo")
-const cors = require("cors")
+// const cors = require("cors")
 const app = express()
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST'],
+//     credentials: true
+// }));
 app.get('/', (req, res) => {
     res.send("Deepak Bhagat");
 });
